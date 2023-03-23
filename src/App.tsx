fbdbd1d3bd57ed8ea10f2af8b840fe0f0d2b07e0
zhekablack97 <zhekablack97@yandex.ru>
2023-03-23 13:28:14 +0700

    Create app

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div>
+      <div></div>
+      <div>
+        <div>dsdsd</div>
+        <aside></aside>
+      </div>
+    </div>
+  );
+}
+
+export default App;
